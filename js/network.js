24edbfc2d272775ee1c5a140d2e5e825cd3f5494
juhyung1234567910-sketch <juhyung1234567910@gmail.com>
2026-05-23 11:10:06 +0000

    Deploying to gh-pages from @ juhyung1234567910-sketch/Game-Platformer@b6f6381c8dd9fa66b00e15a282f8272f087fc831 🚀

--- js/network.js
+++ js/network.js
@@ -5,7 +5,7 @@ import { API_BASE } from './auth.js';
 
 // Socket.IO 클라이언트 CDN (서버에서 자동 제공하거나 CDN 사용)
 // 서버가 같은 오리진이면 /socket.io/socket.io.js 로 자동 제공됨
-const SOCKET_URL = 'http://82.70.222.194';
+const SOCKET_URL = 'https://thegreatestdomain.duckdns.org';
 
 const SEND_INTERVAL  = 10;
 const STALE_TIMEOUT  = 10_000;
